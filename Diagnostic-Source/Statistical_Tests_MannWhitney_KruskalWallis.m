aa92890d3cc0786d93bb6c4c72145f02e7a9be71
U-Tori-HP\Tori <vlw27@cornell.edu>
2015-01-20 21:09:29 -0500

    update Stats code to be consistent with blog

--- Diagnostic-Source/Statistical_Tests_MannWhitney_KruskalWallis.m
+++ Diagnostic-Source/Statistical_Tests_MannWhitney_KruskalWallis.m
@@ -12,29 +12,46 @@
 clc; clear all;
 
 algorithms = {'Borg', 'eMOEA', 'eNSGAII', 'NSGAII', 'MOEAD', 'GDE3'};
-work = sprintf('./SOW6_local_ref/'); %getenv('WORK');
+seeds = (1:1:50);
+metrics = {'GenDist'; 'EpsInd'; 'Hypervolume';};
+%work = sprintf('./working_directory/'); %specify the working directory
 problem = 'MyLake4ObjStoch';
-        
-for i = 1:length(algorithms)
-    name = sprintf('%s_%s.localref.metrics', char(algorithms(i)), problem);
-    results = load(strcat(work, name), '-ascii');
-    Hypervolume(:,i) = results(:,1);
-    Generational_Distance(:,i) = results(:,2);
-    Additive_Epsilon_Indicator(:,i) = results(:,5);
-    
+
+%Loop through metrics
+for i=1:length(metrics)
+    %Loop through algorithms
+    for j=1:length(algorithms)
+        %Loop through seeds
+        for k=1:length(seeds)
+         %open and read files
+            filename = ['./' metrics{i} '_' num2str(75) '_' algorithms{j} '_' num2str(seeds(k)) '.txt'];
+            fh = fopen(filename);
+            if(fh == -1) disp('Error opening analysisFile!'); end
+            values = textscan(fh, '%*s %f', 5, 'Headerlines',1);
+            fclose(fh);
+ 
+            values = values{1};
+ 
+            threshold(k,j,i)       = values(1);
+            best(k,j,i)            = values(2);
+            if strcmp(metrics{i},'Hypervolume'); best(k,j,i)   = best(k,j,i)/(threshold(k,j,i)/(75/100)); end; %Normalize the best Hypervolume value to be between 0 and 1
+            attainment(k,j,i)      = values(3);
+            controllability(k,j,i) = values(4);
+            efficiency(k,j,i)      = values(5);
+        end
+ 
+    end
+ 
 end
 
-[P,ANOVATAB,STATs] = kruskalwallis(Hypervolume,algorithms,'on');
 
-Mann_Whitney_U_results = zeros(length(algorithms),length(algorithms));
+P = kruskalwallis(attainment(:,:,3),algorithms,'off');
 
 for i = 1:length(algorithms)
     for j = 1:length(algorithms)
-    Mann_Whitney_U_results(i,j) = ranksum(Hypervolume(:,i),Hypervolume(:,j),...
+         [P3, Mann_Whitney_U_Hyp(i,j)] = ranksum(attainment(:,i,3),...
+                                     attainment(:,j,3),...
                                   'alpha', 0.05, 'tail', 'right');
     end
-
 end
-
-% xlswrite('test.xlsx',Mann_Whitney_U_results,3);
 
